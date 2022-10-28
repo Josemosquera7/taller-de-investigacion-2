350639547dc6c997dea84fa2c489d8e8b6531d13
DavidChaparro11 <d.chaparro@uniandes.edu.co>
2022-10-27 21:31:01 -0500

    a

--- do-fil.R
+++ do-fil.R
@@ -14,10 +14,12 @@ base_manufacturera_2020_2 <- base_manufacturera_2020_1 %>% rename(actividad_econ
 view(base_manufacturera_2020_2)
 base_manufacturera_2020_3 <- base_manufacturera_2020_2 %>% mutate(total_trabajadores = trabajadores_hombres + trabajadores_mujeres)
 base_manufacturera_final <-  base_manufacturera_2020_3 %>% mutate(proporción_mujeres = trabajadores_mujeres / total_trabajadores)
+base_manufacturera_final [,c(2,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17)]
 view(base_manufacturera_final)
 ##organizamos base de datos de indice departamental de competitividad
-Datos_IDC_1 <- Datos_IDC_1 %>% select(Departamento, Año, `INS-1-1`, `INS-2-2`, `INF-1-3`, `INF-2-1`, `INF-3-1`, `INF-3-2`, `NEG-2-2`,`LAB-1-1`, `LAB-2-1`, `TAM-1-1`, `TAM-2-1`, `INN-1-1`, `Puntaje general` )
+Datos_IDC_1 <- Datos_IDC_1 %>% select(codigo_dane, Departamento, Año, `INS-1-1`, `INS-2-2`, `INF-1-3`, `INF-2-1`, `INF-3-1`, `INF-3-2`, `NEG-2-2`,`LAB-1-1`, `LAB-2-1`, `TAM-1-1`, `TAM-2-1`, `INN-1-1`, `Puntaje general` )
 Datos_IDC_Final <- Datos_IDC_1 %>% rename(gestion_recursos = `INS-1-1`, capacidad_recaudo = `INS-2-2`, Cobertura_energia = `INF-1-3`, vias_primarias = `INF-2-1`, costo_transporte_interno = `INF-3-1`, costo_transporte_aduana = `INF-3-2`, densidad_empresarial = `NEG-2-2`, tasa_glob_participacion = `LAB-1-1`, brecha_laboral_genero = `LAB-2-1`, tamaño_mercado_interno = `TAM-1-1`, tamaño_mercado_externo = `TAM-2-1`, investigacion = `INN-1-1`, IDC = `Puntaje general`)
 view(Datos_IDC_Final)
 ## uniremos bases de datos con un left join co  base en el codigo_dane de departamento
-base_final <-  left_join(x=base_manufacturera_final,y=Datos_IDC_Final,by=c("codigo_dane"))
+base_final <- left_join(x= base_manufacturera_final, y= Datos_IDC_Final, by=c("codigo_dane"))
+view(base_final)
