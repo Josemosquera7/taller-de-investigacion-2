32f9f9818dd7f181a0e42b31455b9cb95fc85b54
DavidChaparro11 <d.chaparro@uniandes.edu.co>
2022-11-02 14:08:31 -0500

    a

--- regresiones_tablas.R
+++ regresiones_tablas.R
@@ -6,11 +6,11 @@ view(base_trabajo)
 #Ahora estimaremos el modelo grande de nuestro proyecto
 reg_1 <- lm(valor_agregado ~  gastostransporte_insumos + gastostransp_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo )
 reg_1
-reg_2 <- lm(log_valagri ~ gastostransporte_insumos + gastostransp_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo)
+reg_2 <- lm(log_valagri ~ log_gastostrans_insumos + log_gastostrans_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo)
 stargazer(reg_1, reg_2,
           type= "html",
           dep.var.labels = c("productividad de la empresa", "logaritmo valor agregado"),
-          covariate.labels = c( "gastos transporte de insumos", "gastos transporte productos", "vias primarias por cada 100 mil habitantes", "indice tamaño mercado interno", "Indice Departamental de Competitividad", "Cobertura de energia"),
+          covariate.labels = c("gastos transporte insumos", "gastos transporte producto", "log gastos transporte insumos", "log gastos transporte producto", "vias primarias por cada 100 mil habitantes", "indice tamaño mercado interno", "Indice Departamental de Competitividad", "Cobertura de energia"),
           out= "resultados del trabajo.doc")
 ##modelo secundario que nos pordria ayudar a responder nuestra pregunta
 reg_3 <- lm(densidad_empresarial ~ vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo )
