80409546210e140e721c5ddf66be7ae485b66b23
DavidChaparro11 <d.chaparro@uniandes.edu.co>
2022-11-02 17:52:17 -0500

    a

--- regresiones_tablas.R
+++ regresiones_tablas.R
@@ -8,9 +8,10 @@ base_trabajo_filtro <- import("base_investigación_filtro_gastostransporte.xlsx"
 reg_1 <- lm(valor_agregado ~  gastostransporte_insumos + gastostransp_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo )
 reg_1
 reg_2 <- lm(valor_agregado ~  gastostransporte_insumos + gastostransp_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo_filtro )
-reg_3 <- lm(log_valagri ~ log_gastostrans_insumos + log_gastostrans_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo)
+reg_3 <- lm(log_valagri ~ log_gastostrans_insumos + log_gastostrans_productos + vias_primarias +  tamaño_mercado_interno + IDC + Cobertura_energia, data = base_trabajo_filtro)
 stargazer(reg_1, reg_2, reg_3,
           type= "html",
           dep.var.labels = c("productividad de la empresa", "logaritmo valor agregado"),
           covariate.labels = c("gastos transporte insumos", "gastos transporte producto", "log gastos transporte insumos", "log gastos transporte producto", "vias primarias por cada 100 mil habitantes", "indice tamaño mercado interno", "Indice Departamental de Competitividad", "Cobertura de energia"),
           out= "resultados del trabajo.doc")
+
